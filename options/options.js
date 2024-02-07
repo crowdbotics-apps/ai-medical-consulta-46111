1872e1d1a3cd1f242708b7375f099715e13526fb
Crowdbotics <team@crowdbotics.com>
2024-02-07 21:43:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ai-medical-consulta-46111",
+  url: "https://ai-medical-consulta-46111.botics.co",
+  api: "https://ai-medical-consulta-46111.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
